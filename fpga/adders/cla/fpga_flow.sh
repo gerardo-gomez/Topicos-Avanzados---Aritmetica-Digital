5c0a1319c184c69bcc80f8513b4079ecafb570f3
Gerardo Gomez <gerardo.gomez@iteso.mx>
2026-06-29 16:54:12 -0600

    Updated CLA Quartus project with automation to compile and generate timing reports

---
+++ fpga/adders/cla/fpga_flow.sh
@@ -0,0 +1,12 @@
+# Description: This script compiles the design in Quartus and generates a detailed timing analysis report
+#
+# To run this script:
+# cd <repo_root>
+# $ source ./fpga/adders/cla/generate_reports.sh
+#
+# Note: Ensure that < Intel® Quartus® Prime directory>/quartus/bin directory is in your PATH environment variable
+#
+# Run compilation and generates timing analysis reports
+quartus_sh.exe --flow compile ./fpga/adders/cla/fpga_top.qpf
+# Reports detailed timing of the first 100 paths
+quartus_sta.exe -t ./fpga/adders/cla/report_timing.tcl
