19e06ea3e56ea53eb25bafaf5064b95480d2cb0a
Gerardo Gomez <gerardo.gomez@iteso.mx>
2026-07-02 16:15:29 -0600

    Refactored booth4_mul and fixed some bugs

--- design/multipliers/booth4_mul.sv
+++ design/multipliers/booth4_mul.sv
@@ -20,15 +20,16 @@ module multiplier #(
   localparam int BOOTH_NUM_PP        = SRCB_WIDTH / 2;          // Number of partial products for radix-4
   localparam int BOOTH_SHIFT_AMOUNT  = $clog2(BOOTH_RADIX);     // Amount to shift each partial product. 2 for radix-4
 
-  logic                   [ SRCA_WIDTH        -1:0] srca_comp2;
-  logic [BOOTH_NUM_PP-1:0][ BOOTH_TRIPLET_SIZE-1:0] booth_triplet;
-  logic [BOOTH_NUM_PP-1:0][(SRCA_WIDTH + 1)   -1:0] booth_pp;
-  logic [BOOTH_NUM_PP-1:0]                          booth_pp_sign;
-  logic [BOOTH_NUM_PP-1:0][ RESULT_WIDTH      -1:0] booth_pp_shifted;
+  typedef logic [BOOTH_TRIPLET_SIZE-1:0] t_booth_triplet;
+
+  logic                             [SRCA_WIDTH  -1:0] srca_comp2;
+  t_booth_triplet [BOOTH_NUM_PP-1:0]                   booth_triplet;
+  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0] booth_pp;
+  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0] booth_pp_shifted;
 
   // Function that groups the bits of srcb into triplets for radix-4 Booth encoding
   // Triplets are formed with overlap of 1 bit. Example for SRCB_WIDTH=8: (b_7 b_6 b_5), (b_5 b_4 b_3), (b_3 b_2 b_1), (b_1 b_0 b_(-1))
-  function automatic logic [BOOTH_TRIPLET_SIZE-1:0] get_booth_triplet(input int pp_idx, input logic [SRCB_WIDTH-1:0] srcb);
+  function automatic t_booth_triplet f_get_booth_triplet(input int pp_idx, input logic [SRCB_WIDTH-1:0] srcb);
     logic [(SRCB_WIDTH + 1)-1:0] srcb_ext;
     // Extend srcb with auxiliary 0 at LSB for triplet grouping (b_(-1) = 0)
     srcb_ext = {srcb, 1'b0};
@@ -36,26 +37,23 @@ module multiplier #(
   endfunction
 
   // Function that returns a booth partial product based on the encoded triplet
-  // Table of encoded triplets to partial products (radix-4)
-  function automatic logic [(SRCA_WIDTH + 1)-1:0] get_booth_pp(input logic [BOOTH_TRIPLET_SIZE-1:0] triplet, input logic [SRCA_WIDTH-1:0] srca, input logic [SRCA_WIDTH-1:0] srca_comp2);
-    unique case (triplet)                                // b_(i+1), b_i, b_(i-1)
-      3'b000: return (SRCA_WIDTH + 1)'(0              ); //       0,   0,   0 -> 0
-      3'b001: return (SRCA_WIDTH + 1)'(srca           ); //       0,   0,   1 -> +1A
-      3'b010: return (SRCA_WIDTH + 1)'(srca           ); //       0,   1,   0 -> +1A
-      3'b011: return (SRCA_WIDTH + 1)'(srca       << 1); //       0,   1,   1 -> +2A
-      3'b100: return (SRCA_WIDTH + 1)'(srca_comp2 << 1); //       1,   0,   0 -> -2A
-      3'b101: return (SRCA_WIDTH + 1)'(srca_comp2     ); //       1,   0,   1 -> -1A
-      3'b110: return (SRCA_WIDTH + 1)'(srca_comp2     ); //       1,   1,   0 -> -1A
-      3'b111: return (SRCA_WIDTH + 1)'(0              ); //       1,   1,   1 -> 0
-    endcase
-  endfunction
-
-  // Function that returns the sign of a booth partial product given encoded triplet
-  function automatic logic get_booth_pp_sign(input logic [BOOTH_TRIPLET_SIZE-1:0] triplet, input logic srca_sign);
+  // Table of encoded triplets to partial products (radix-4):
+  //   b_(i+1), b_i, b_(i-1)
+  //         0,   0,   0 -> 0
+  //         0,   0,   1 -> +1A
+  //         0,   1,   0 -> +1A
+  //         0,   1,   1 -> +2A
+  //         1,   0,   0 -> -2A
+  //         1,   0,   1 -> -1A
+  //         1,   1,   0 -> -1A
+  //         1,   1,   1 -> 0
+  function automatic logic [RESULT_WIDTH-1:0] f_get_booth_pp(input t_booth_triplet triplet, input logic [SRCA_WIDTH-1:0] srca, input logic [SRCA_WIDTH-1:0] srca_comp2);
     unique case (triplet)
-      3'b000, 3'b111:         return 1'b0;
-      3'b001, 3'b010, 3'b011: return srca_sign;
-      3'b100, 3'b101, 3'b110: return ~srca_sign;
+      3'b000, 3'b111: return '0;                                                                                         // 0A
+      3'b001, 3'b010: return {{ SRCB_WIDTH   {srca      [SRCA_WIDTH-1] & is_signed}},                 srca            }; // +A
+      3'b011:         return {{(SRCB_WIDTH-1){srca      [SRCA_WIDTH-1] & is_signed}}, (SRCA_WIDTH+1)'(srca       << 1)}; // +2A
+      3'b100:         return {{(SRCB_WIDTH-1){srca_comp2[SRCA_WIDTH-1] & is_signed}}, (SRCA_WIDTH+1)'(srca_comp2 << 1)}; // -2A
+      3'b101, 3'b110: return {{ SRCB_WIDTH   {srca_comp2[SRCA_WIDTH-1] & is_signed}},                 srca_comp2      }; // -A
     endcase
   endfunction
 
@@ -65,20 +63,17 @@ module multiplier #(
 
     for (int pp_idx = 0; pp_idx < BOOTH_NUM_PP; pp_idx++) begin
       // Group srcb bits into triplets
-      booth_triplet[pp_idx] = get_booth_triplet(pp_idx, srcb);
+      booth_triplet[pp_idx] = f_get_booth_triplet(pp_idx, srcb);
 
       // Get the partial products based on the encoded triplets
-      booth_pp[pp_idx] = get_booth_pp(booth_triplet[pp_idx], srca, srca_comp2);
-
-      // Get the sign of the partial products
-      booth_pp_sign[pp_idx] = get_booth_pp_sign(booth_triplet[pp_idx], srca[SRCA_WIDTH-1]);
+      booth_pp[pp_idx] = f_get_booth_pp(booth_triplet[pp_idx], srca, srca_comp2);
     end
   end
 
-  // Sign-extend and shift the partial products according to their weight
+  // Shift the partial products according to their weight
   generate
     for (genvar pp_idx = 0; pp_idx < BOOTH_NUM_PP; pp_idx++) begin : gen_pp_shifted
-      assign booth_pp_shifted[pp_idx] = {{(SRCB_WIDTH - (pp_idx * BOOTH_SHIFT_AMOUNT)){booth_pp_sign[pp_idx]}}, booth_pp[pp_idx][SRCA_WIDTH-1:0], {(pp_idx * BOOTH_SHIFT_AMOUNT){1'b0}}};
+      assign booth_pp_shifted[pp_idx] = {booth_pp[pp_idx][(RESULT_WIDTH - (pp_idx * BOOTH_SHIFT_AMOUNT))-1:0], {(pp_idx * BOOTH_SHIFT_AMOUNT){1'b0}}};
     end : gen_pp_shifted
   endgenerate
 
