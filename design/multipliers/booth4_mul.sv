bd6c43a39f2dc8d3d7c68173a957f7a60adce58c
Gerardo Gomez <gerardo.gomez@iteso.mx>
2026-07-02 19:27:14 -0600

    Booth radix-4 working with signed numbers

--- design/multipliers/booth4_mul.sv
+++ design/multipliers/booth4_mul.sv
@@ -19,13 +19,17 @@ module multiplier #(
   localparam int BOOTH_PP_TABLE_SIZE = 2**BOOTH_TRIPLET_SIZE;   // Number of encoded triplets to partial products. 2^3 = 8 for radix-4
   localparam int BOOTH_NUM_PP        = SRCB_WIDTH / 2;          // Number of partial products for radix-4
   localparam int BOOTH_SHIFT_AMOUNT  = $clog2(BOOTH_RADIX);     // Amount to shift each partial product. 2 for radix-4
+  localparam int BOOTH_PP_MAX_MULT   = 2;                       // Maximum multiplier for partial products. 2 for radix-4
 
   typedef logic [BOOTH_TRIPLET_SIZE-1:0] t_booth_triplet;
 
-  logic                             [SRCA_WIDTH  -1:0] srca_comp2;
-  t_booth_triplet [BOOTH_NUM_PP-1:0]                   booth_triplet;
-  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0] booth_pp;
-  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0] booth_pp_shifted;
+  t_booth_triplet [BOOTH_NUM_PP-1:0]                        triplet;
+  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0]      pp;
+  logic           [BOOTH_NUM_PP-1:0][RESULT_WIDTH-1:0]      pp_shifted;
+  logic           [BOOTH_NUM_PP-1:0]                        pp_is_comp2;
+  logic           [BOOTH_NUM_PP-1:0]                        pp_is_mult2;
+  logic           [BOOTH_NUM_PP-1:0][BOOTH_PP_MAX_MULT-1:0] pp_comp2_corr;
+  logic                             [RESULT_WIDTH-1:0]      comp2_corr;
 
   // Function that groups the bits of srcb into triplets for radix-4 Booth encoding
   // Triplets are formed with overlap of 1 bit. Example for SRCB_WIDTH=8: (b_7 b_6 b_5), (b_5 b_4 b_3), (b_3 b_2 b_1), (b_1 b_0 b_(-1))
@@ -47,42 +51,74 @@ module multiplier #(
   //         1,   0,   1 -> -1A
   //         1,   1,   0 -> -1A
   //         1,   1,   1 -> 0
-  function automatic logic [RESULT_WIDTH-1:0] f_get_booth_pp(input t_booth_triplet triplet, input logic [SRCA_WIDTH-1:0] srca, input logic [SRCA_WIDTH-1:0] srca_comp2);
+  // Note: Partial products don't use the complete 2's complement of srca.
+  //       Instead, 1's complement is used and the result is corrected by injecting constant 1s to the final sum of partial products.
+  function automatic logic [RESULT_WIDTH-1:0] f_get_booth_pp(input t_booth_triplet triplet, input logic [SRCA_WIDTH-1:0] srca);
+    logic [RESULT_WIDTH-1:0] srca_sign_ext = {{SRCB_WIDTH{srca[SRCA_WIDTH-1]}}, srca};
     unique case (triplet)
-      3'b000, 3'b111: return '0;                                                                                         // 0A
-      3'b001, 3'b010: return {{ SRCB_WIDTH   {srca      [SRCA_WIDTH-1] & is_signed}},                 srca            }; // +A
-      3'b011:         return {{(SRCB_WIDTH-1){srca      [SRCA_WIDTH-1] & is_signed}}, (SRCA_WIDTH+1)'(srca       << 1)}; // +2A
-      3'b100:         return {{(SRCB_WIDTH-1){srca_comp2[SRCA_WIDTH-1] & is_signed}}, (SRCA_WIDTH+1)'(srca_comp2 << 1)}; // -2A
-      3'b101, 3'b110: return {{ SRCB_WIDTH   {srca_comp2[SRCA_WIDTH-1] & is_signed}},                 srca_comp2      }; // -A
+      3'b000, 3'b111: return                    '0; // 0A
+      3'b001, 3'b010: return   srca_sign_ext      ; // +A
+      3'b011:         return  (srca_sign_ext << 1); // +2A
+      3'b100:         return ~(srca_sign_ext << 1); // -2A
+      3'b101, 3'b110: return ~(srca_sign_ext     ); // -A
+    endcase
+  endfunction
+
+  function automatic logic f_is_booth_pp_comp2(input t_booth_triplet triplet);
+    unique case (triplet)
+      3'b100, 3'b101, 3'b110:                 return 1'b1; // -A, -2A
+      3'b000, 3'b001, 3'b010, 3'b011, 3'b111: return 1'b0; // +A, +2A or 0
+    endcase
+  endfunction
+
+  function automatic logic f_is_booth_pp_mult2(input t_booth_triplet triplet);
+    unique case (triplet)
+      3'b011, 3'b100:                                 return 1'b1; // +2A, -2A
+      3'b000, 3'b001, 3'b010, 3'b101, 3'b110, 3'b111: return 1'b0; // +A, -A or 0
     endcase
   endfunction
 
   always_comb begin
-    // 2's complement of srca
-    srca_comp2 = ~srca + 1'b1;
-
     for (int pp_idx = 0; pp_idx < BOOTH_NUM_PP; pp_idx++) begin
       // Group srcb bits into triplets
-      booth_triplet[pp_idx] = f_get_booth_triplet(pp_idx, srcb);
+      triplet[pp_idx] = f_get_booth_triplet(pp_idx, srcb);
 
       // Get the partial products based on the encoded triplets
-      booth_pp[pp_idx] = f_get_booth_pp(booth_triplet[pp_idx], srca, srca_comp2);
+      pp[pp_idx] = f_get_booth_pp(triplet[pp_idx], srca);
+
+      // Compute the partial product correction for 2's complement
+      // Detect if the partial product is a 2's complement and needs correction
+      pp_is_comp2[pp_idx] = f_is_booth_pp_comp2(triplet[pp_idx]);
+      // Detect if the partial product is a 2x multiplier
+      pp_is_mult2[pp_idx] = f_is_booth_pp_mult2(triplet[pp_idx]);
+
+      // Assign the position of the partial product correction
+      pp_comp2_corr[pp_idx][0] = pp_is_comp2[pp_idx];
+      pp_comp2_corr[pp_idx][1] = '0;
+//    pp_comp2_corr[pp_idx][0] = pp_is_comp2[pp_idx] & (~pp_is_mult2[pp_idx]); // - A
+//    pp_comp2_corr[pp_idx][1] = pp_is_comp2[pp_idx] &   pp_is_mult2[pp_idx];  // -2A
     end
   end
 
   // Shift the partial products according to their weight
   generate
     for (genvar pp_idx = 0; pp_idx < BOOTH_NUM_PP; pp_idx++) begin : gen_pp_shifted
-      assign booth_pp_shifted[pp_idx] = {booth_pp[pp_idx][(RESULT_WIDTH - (pp_idx * BOOTH_SHIFT_AMOUNT))-1:0], {(pp_idx * BOOTH_SHIFT_AMOUNT){1'b0}}};
+      localparam int PP_SHIFT_AMOUNT = pp_idx * BOOTH_SHIFT_AMOUNT;
+
+      assign pp_shifted[pp_idx] = {pp[pp_idx][(RESULT_WIDTH - PP_SHIFT_AMOUNT)-1:0], {PP_SHIFT_AMOUNT{1'b0}}};
+
+      assign comp2_corr[PP_SHIFT_AMOUNT +: 2] = pp_comp2_corr[pp_idx];
     end : gen_pp_shifted
   endgenerate
+  assign comp2_corr[RESULT_WIDTH-1:BOOTH_NUM_PP*BOOTH_SHIFT_AMOUNT] = '0; // Zero the rest of the bits of the 2's complement correction
 
   // Sum of all partial products
   always_comb begin
     result = 0;
     for (int pp_idx = 0; pp_idx < BOOTH_NUM_PP; pp_idx++) begin
-      result += booth_pp_shifted[pp_idx];
+      result += pp_shifted[pp_idx];
     end
+    result += comp2_corr; // Add the 2's complement correction
   end
 
 endmodule
