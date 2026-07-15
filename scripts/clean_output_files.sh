1312cc132133261deb206639a420538b5b123e82
Gerardo Gomez <gerardo.gomez@iteso.mx>
2026-07-15 14:43:53 -0600

    Updated auxiliary script

--- scripts/clean_output_files.sh
+++ scripts/clean_output_files.sh
@@ -1,3 +1,3 @@
 cd $1
-rm *.asm.rpt *.done *.eda.rpt *.fit.smsg *.flow.rpt *.jdi *.pin *.pof *.sld *.sof
+rm *.asm.rpt *.done *.eda.rpt *.fit.smsg *.flow.rpt *.jdi *.pin *.pof *.sld *.sof *.jditmp
 cd -
